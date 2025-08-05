2c878d6b49e4d694491d7a7a69b748d2bc44aff1
azizftw7 <benzarbaziz.2015@gmail.com>
2025-08-05 17:02:58 +0100

    [ADD] the intialization of splash screen

---
+++ app/src/main/java/com/example/ingame/view/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package com.example.ingame.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ingame.MainActivity;
+import com.example.ingame.R;
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+    }
+}
+        , 1500);}
+}
