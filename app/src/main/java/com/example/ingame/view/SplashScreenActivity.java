0cba5c758d38c11449be670e9782465507b6f3d0
azizftw7 <benzarbaziz.2015@gmail.com>
2025-08-08 17:27:26 +0100

    [ADD] added animation and fixed the menu and bottom bar to my components

--- app/src/main/java/com/example/ingame/view/SplashScreenActivity.java
+++ app/src/main/java/com/example/ingame/view/SplashScreenActivity.java
@@ -1,19 +1,31 @@
 package com.example.ingame.view;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ingame.MainActivity;
 import com.example.ingame.R;
+@SuppressLint("CustomSplashScreen")
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ImageView logo = findViewById(R.id.logo);
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logo.startAnimation(fadeIn);
+        TextView textView = findViewById(R.id.textView);
+        Animation ffadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        textView.startAnimation(ffadeIn);
         new Handler().postDelayed(() -> {
             Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
             startActivity(intent);
