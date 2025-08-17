84e57488b9307b8be7c6bb975a63302820bb3d10
azizftw7 <benzarbaziz.2015@gmail.com>
2025-08-17 17:22:15 +0100

    [ADD] removed the action bar and now creating a custom one and styling the home fragment

--- app/src/main/java/com/example/ingame/MainActivity.java
+++ app/src/main/java/com/example/ingame/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_members, R.id.navigation_attendance, R.id.navigation_settings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
