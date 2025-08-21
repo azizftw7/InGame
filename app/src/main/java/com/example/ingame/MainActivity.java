21a98d83ccd3c269c217a7dfa532c082da2a31c8
azizftw7 <benzarbaziz.2015@gmail.com>
2025-08-21 20:32:44 +0100

    [ADD] adding better ui for the action bar and including it in main

--- app/src/main/java/com/example/ingame/MainActivity.java
+++ app/src/main/java/com/example/ingame/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ingame;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -22,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        TextView actionBar=binding.customActionBar.actionb;
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -30,7 +31,18 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_members, R.id.navigation_attendance, R.id.navigation_settings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            int id = destination.getId();
+            if (id == R.id.navigation_home) {
+                binding.customActionBar.actionb.setText("Home");
+            } else if (id == R.id.navigation_members) {
+                binding.customActionBar.actionb.setText("Members");
+            } else if (id == R.id.navigation_attendance) {
+                binding.customActionBar.actionb.setText("Attendance");
+            } else if (id == R.id.navigation_settings) {
+                binding.customActionBar.actionb.setText("Settings");
+            }
+        });
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
